46423cdc00e0157ce3110532f453017c9fb81bbd
Eliii29 <165733816+Eliii29@users.noreply.github.com>
2025-04-20 02:16:42 +0800

    Add files via upload

---
+++ estetika/src/pages/NotificationPage.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import '../styles/Layout.css';
+function Notification(){
+    return(
+        <>
+        <h1 className="article-list">NOtif</h1><ul>
+            <li>Confessions</li>
+            <li>Who's</li>
+            <li>the</li>
+            <li>unlucky</li>
+            <li>guy</li>
+        </ul></>
+    );
+}
+
+export default Notification;
