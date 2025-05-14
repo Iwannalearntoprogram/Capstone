da0e84c856797a3fcbe5da227fe746853a9d1750
Sean <2136seanpaul@gmail.com>
2025-05-14 21:20:12 +0800

    Refactor User Model

--- estetika/backend/models/User.js
+++ estetika/backend/models/User.js
@@ -1,20 +1,37 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phoneNumber: { type: String },
-  userType: {
-    type: String,
-    enum: ["admin", "designer", "client"],
-    default: "client",
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+    },
+    password: { type: String, required: [true, "Password is required"] },
+    phoneNumber: { type: String },
+    userType: {
+      type: String,
+      enum: ["admin", "designer", "client"],
+      default: "client",
+    },
+    emailVerified: { type: Boolean, default: false },
+    otp: { type: String },
+    otpExpiresAt: { type: Date },
   },
-  createdAt: { type: Date, default: Date.now },
-  emailVerified: { type: Boolean, default: false },
-  otp: { type: String },
-  otpExpiresAt: { type: Date },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
