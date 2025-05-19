b317b0d1718416d89ea398059250c9e4ad46f669
Sean <2136seanpaul@gmail.com>
2025-05-19 19:02:57 +0800

    Add Logout Route

--- estetika/backend/routes/User/authRoute.js
+++ estetika/backend/routes/User/authRoute.js
@@ -5,11 +5,14 @@ const {
   login,
   verifyEmail,
   verifyOTP,
+  logout,
 } = require("../../controllers/User/AuthController");
+const checkAuth = require("../../utils/checkAuth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/send-otp", verifyEmail);
 router.post("/verify-otp", verifyOTP);
+router.get("/logout", checkAuth, logout);
 
 module.exports = router;
