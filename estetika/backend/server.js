540ad893a849a79e96ee2c265d09254e08270b25
Sean <2136seanpaul@gmail.com>
2025-05-23 01:26:13 +0800

    Fix Cron Job Date Error

--- estetika/backend/server.js
+++ estetika/backend/server.js
@@ -2,6 +2,23 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const initSocket = require("./utils/socket");
 
+dotenv.config();
+
+const app = require("./app");
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ MongoDB connection error:", err));
+
+const server = app.listen(PORT, async () => {
+  console.log("Server Started " + process.env.PORT);
+  console.log(process.env.NODE_ENV);
+});
+
+initSocket(server);
+
 process.on("uncaughtException", (err) => {
   console.log("Uncaught Exception!!! 💣 Shutting down...");
   console.log(err.name, err.message, err);
@@ -19,20 +36,3 @@ process.on("unhandledRejection", (err) => {
     process.exit(1);
   });
 });
-
-dotenv.config();
-
-const app = require("./app");
-const PORT = process.env.PORT;
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.log("❌ MongoDB connection error:", err));
-
-const server = app.listen(PORT, async () => {
-  console.log("Server Started " + process.env.PORT);
-  console.log(process.env.NODE_ENV);
-});
-
-initSocket(server);
