bb9096ee93d5d4bea07973be65753683b00b23fb
Sean Paul Monton <2136seanpaul@gmail.com>
2025-09-11 21:38:00 +0800

    add dynamic customer satisfaction chart

--- estetika/backend/routes/Project/ratingRoute.js
+++ estetika/backend/routes/Project/ratingRoute.js
@@ -5,11 +5,13 @@ const {
   rating_post,
   rating_delete,
   rating_stats,
+  rating_stats_all,
 } = require("../../controllers/Project/RatingController");
 const checkAuth = require("../../utils/checkAuth");
 
 router.get("/", rating_get);
 router.get("/stats", rating_stats);
+router.get("/stats/all", rating_stats_all);
 router.post("/", checkAuth, rating_post);
 router.delete("/", checkAuth, rating_delete);
 
